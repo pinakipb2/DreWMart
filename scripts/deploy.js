5987a28d146173687e5f49b635a0e9b684ba990a
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-11-13 14:59:09 +0530

    Added auto-update .env on contract deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,8 +5,23 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 
+const fs = require('fs');
+const os = require('os');
+
 const axios = require('axios');
 const { ethers } = require('hardhat');
+
+function setEnvValue(key, value) {
+  // read file from hdd & split if from a linebreak to a array
+  const ENV_VARS = fs.readFileSync('./.env', 'utf8').split(os.EOL);
+  // find the env we want based on the key
+  const target = ENV_VARS.indexOf(ENV_VARS.find((line) => line.match(new RegExp(key))));
+  // replace the key/value with the new value
+  ENV_VARS.splice(target, 1, `${key}=${value}`);
+  // write everything back to the file system
+  fs.writeFileSync('./.env', ENV_VARS.join(os.EOL));
+  console.log(`${key}=${value}`);
+}
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -21,15 +36,17 @@ async function main() {
   } catch (err) {
     console.log(err.response.data);
   }
-  console.log('Account balance:', (await deployer.getBalance()).toString());
+  console.log('Account Balance:', (await deployer.getBalance()).toString());
   const DreWMartToken = await ethers.getContractFactory('DreWMartToken');
   const DMTK = await DreWMartToken.deploy();
-  console.log('DMTK address:', DMTK.address);
-  console.log('Account balance:', (await deployer.getBalance()).toString());
+  console.log('DMTK Address:', DMTK.address);
+  setEnvValue('NEXT_PUBLIC_DMTK_ADDRESS', DMTK.address);
+  console.log('Account Balance:', (await deployer.getBalance()).toString());
   const DreWToken = await ethers.getContractFactory('DreWToken');
   const DTK = await DreWToken.deploy();
-  console.log('DTK address:', DTK.address);
-  console.log('Account balance:', (await deployer.getBalance()).toString());
+  console.log('DTK Address:', DTK.address);
+  setEnvValue('NEXT_PUBLIC_DTK_ADDRESS', DTK.address);
+  console.log('Account Balance:', (await deployer.getBalance()).toString());
 }
 
 main().catch((error) => {
